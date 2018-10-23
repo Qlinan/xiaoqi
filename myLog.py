e2d298dca67a32677d6108615ff29b1f7f1edfc4
Qlinan <43258555+Qlinan@users.noreply.github.com>
2018-10-23 10:33:58 +0800

    Add files via upload

---
+++ myLog.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 19 10:39:15 2018
+
+@author: Tarena
+"""
+import loggig
+import sys
+class LoggerHelper:
+    def __init__(self,name):
+        pass
+    def writeLog(self,level=logging.DEBUG,message):
+        pass
+    
+    def removeLog(self):
+        pass
